ee785b49f5b5a34cec22f66891af6cc4e840f193
willfaust <39139570+willfaust@users.noreply.github.com>
2021-02-06 07:26:44 +0000

    Update titlekeystermux.sh

--- titlekeystermux.sh
+++ titlekeystermux.sh
@@ -1,16 +1,20 @@
 termux-setup-storage
 
+pkg update -y
+pkg install vim
+
 rm -f title.keys, ~/storage/shared/Documents/title.keys # removes any old title.keys file
 
 shopt -s globstar # enable globstar
 
 for i in ~/storage/shared/**/*.nsp
-do 
-	./hactool -t pfs0 "$i" --outdir output
-	title=$(xxd output/*.tik | grep -oP '(?<=000002a0: ).{39}' | sed 's/ //g')
-	key=$(xxd output/*.tik | grep -oP '(?<=00000180: ).{39}' | sed 's/ //g')
-	echo $title=$key >> title.keys
-	rm -r -f output
+do
+	reference=$(xxd "$i" | grep -oP -m 1 ".{8}(?=.{43}Root-CA00000003-)")
+	titleloc=$(printf "%x\n" $((16#$reference + 16#160)))
+	keyloc=$(printf "%x\n" $((16#$reference + 16#40)))
+	title=$(xxd "$i" | grep -oP -m 1 "(?<=$titleloc: ).{39}" | sed 's/ //g')
+	key=$(xxd "$i" | grep -oP -m 1 "(?<=$keyloc: ).{39}" | sed 's/ //g')
+	echo $title=$key >> ~/Downloads/title.keys
 done
 
 cp title.keys ~/storage/shared/Documents/title.keys

8baf78719f35f0e929d160fceac9de632bac8780
willfaust <39139570+willfaust@users.noreply.github.com>
2021-02-06 02:27:57 +0000

    Create hactooltermux.sh

---
+++ hactooltermux.sh
@@ -0,0 +1,14 @@
+rm -r -f hactool # removes any previous hactool instance
+
+pkg update -y
+pkg install git make clang binutils -y
+
+git clone https://github.com/SciresM/hactool
+mv hactool hactoolsrc
+cd hactoolsrc
+cp config.mk.template config.mk
+make
+mv hactool ..
+cd ..
+rm -r -f hactoolsrc
+./hactool
